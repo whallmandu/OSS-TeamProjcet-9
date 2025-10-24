2146673adfa4bb8bb3080c5a8f06335a85563b4f
woojinKIng777 <kim3bae0@gmail.com>
2025-10-24 13:14:45 +0900

    Feat main.c: add new feature
    else if(n == 2025046027) printf("Kim WooJin); 추가했습니다.

--- main.c
+++ main.c
@@ -5,6 +5,7 @@ int main() {
   printf("Your student ID: ");
   scanf("%d", &n);
   if(n == 2025046018) printf("Your name is Kim RaeHyeon");
+  else if(n == 2025046027) printf("Your name is Kim WooJin");
 
   return 0;
 }
