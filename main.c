846f0c5f832f559253815993087dbc40f389eb9b
ya-e-nic <kjh20060829@gmail.com>
2025-10-24 17:22:13 +0900

    Feat main.c: add new feature
    Add student ID

--- main.c
+++ main.c
@@ -7,6 +7,8 @@ int main() {
   if(n == 2025046018) printf("Your name is Kim RaeHyeon");
   else if(n == 2025046027) printf("Your name is Kim WooJin");
   else if (n == 2025046022) printf("Your name is Hong TaeGyeong");
+  else if (n == 2025046032) printf("Your name is Kim jungHun");
+  else printf("can't search student ID"):
 
   return 0;
 }
