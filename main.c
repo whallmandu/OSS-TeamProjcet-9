a09c8212ff3924072e09aaf81954de8cece00409
woojinKIng777 <kim3bae0@gmail.com>
2025-11-09 23:08:18 +0900

    intro 도입부 작성

--- main.c
+++ main.c
@@ -1,15 +1,11 @@
 #include <stdio.h>
 
+
+
 int main() {
-  int n;
-  printf("Your student ID: ");
-  scanf("%d", &n);
-  if(n == 2025046018) printf("Your name is Kim RaeHyeon");
-  else if(n == 2025046027) printf("Your name is Kim WooJin");
-  else if (n == 2025046022) printf("Your name is Hong TaeGyeong");
-  else if (n == 2025046032) printf("Your name is Kim jungHun");
-  else if (n == 2025088131 ) printf("Yuur Name is Shin DongUk');
-  else printf("can't search student ID"):
+ 
+  printf("무인도에서 살아남기 게임에 오신 것을 환영합니다\n");
+
 
   return 0;
 }
